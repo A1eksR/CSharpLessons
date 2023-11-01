c2cb7de77e039534fe7b5c2a9f16fb566d182f02
A1eksR <147248309+A1eksR@users.noreply.github.com>
2023-11-01 20:05:02 +0200

    day12

---
+++ Day12/Day12/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] masivs = { 1, 2, 3, 4, 5, 6 };
+        Tasks.MetodeKlaseTasks(masivs);
+        Console.ReadLine();
+    }
+
+}
+
---
+++ Day12/Day12/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Day12
+{
+	public class Tasks
+	{
+		public Tasks()
+		{
+		}
+
+		public static void MetodeKlaseTasks(int[] masivs)
+		{
+			List<int> lists = new List<int>();
+			for (int i = masivs.Length - 1; i >= 0; i--)
+			{
+				lists.Add(masivs[i]);
+			}
+			Console.WriteLine(lists);
+		}
+
+
+	}
+
+}
+
