821a70bdf2c78061e6de407b091fde5240dbc786
A1eksR <147248309+A1eksR@users.noreply.github.com>
2023-11-11 13:34:43 +0200

    day13

--- Day12MD/Day12MD/Book.cs
+++ Day12MD/Day12MD/Book.cs
@@ -3,8 +3,33 @@ namespace Day12MD
 {
 	public class Book
 	{
-		public Book()
+		public String name;
+		public String authorName;
+		public String authorLastName;
+		public int year;
+		public int pirmsCikGadiemIzdota;
+
+		public void PirmsCikGadiem(int pirmsCikGadiemIzdota)
 		{
+			int gramataIzdotaGadi = 2023 - pirmsCikGadiemIzdota;
+		}
+
+		public Book(String name, String authorName, String authorLastName, int year, int gramataIzdotaGadi)
+		{
+			this.name = name;
+			this.authorName = authorLastName;
+			this.authorLastName = authorLastName;
+			this.year = year;
+			this.pirmsCikGadiemIzdota = gramataIzdotaGadi;
+		}
+
+		public void IzvaditInfo()
+		{
+			Console.WriteLine("gramatas nosaukums ir : " + name);
+			Console.WriteLine("gramatas autora vards ir : " + authorName);
+			Console.WriteLine("gramatas autora uzvafrds ir : " + authorLastName);
+			Console.WriteLine("gramatas publikacijas gads ir : " + year);
+			Console.WriteLine("gramata tika izdota pirms " + pirmsCikGadiemIzdota + " gadiem");
 		}
 	}
 }
--- Day12MD/Day12MD/Program.cs
+++ Day12MD/Day12MD/Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Book book1 = new Book("Lord of the rings, ", "Jhon ", "Tolkien, ", 1937, ?);
+        book1.IzvaditInfo();
+
+        Book book2 = new Book("the little prince, ", "Antoine ", "Saint-Exupery, ", 1943, ?);
+        book2.IzvaditInfo();
+
+        Book book3 = new Book("1984, ", "George ", "Orwell, ", 1949, ?);
+        book3.IzvaditInfo();
+
+        
     }
 }
 
