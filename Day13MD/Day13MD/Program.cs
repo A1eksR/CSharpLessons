851335820060cf3f545f637e413f2c007e58a420
A1eksR <147248309+A1eksR@users.noreply.github.com>
2023-11-20 13:34:43 +0200

    day13md

---
+++ Day13MD/Day13MD/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Day13MD;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<String> lists = new List<String>();
+            for(int i =0; i < 5; i++)
+            {
+                String ievaditSimbolu = Console.ReadLine();
+                lists.Add(ievaditSimbolu);
+            }
+            Console.ReadLine();
+        }
+        catch
+        {
+            Console.WriteLine("Error");
+        }
+    }
+
+    public static void IeliktFila(List<String> lists)
+    {
+        try
+        {
+
+            String desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            String fullName = Path.Combine(desktopPath, lists);
+
+            StreamWriter file = new StreamWriter(fullName, false);
+            for (int i = 0; i < lists.Count; i++)
+            {
+                file.WriteLine(lists[i]);
+            }
+            file.Flush();
+            file.Dispose();
+        }
+        catch
+        {
+            Console.WriteLine("Error");
+        }
+    }
+
+    public static void LasitFile(StreamWriter file, List<String> lists)
+    {
+        try
+        {
+
+            String desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            String fullName = Path.Combine(desktopPath, lists);
+
+            StreamReader sr = new StreamReader(fullName);
+
+            String line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error!");
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
+
