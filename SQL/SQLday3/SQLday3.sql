08426594dc98fcf778a396342747a9d358010179
A1eksR <147248309+A1eksR@users.noreply.github.com>
2023-12-09 13:19:06 +0200

    fgf

--- SQL/SQLday3/SQLday3.sql
+++ SQL/SQLday3/SQLday3.sql
@@ -7,7 +7,22 @@ WHERE artists.ArtistId = albums.ArtistId;
 
 SELECT artists.ArtistId, albums.ArtistId, tracks.AlbumId
 FROM artists
-INNER JOIN albums
-ON artists.ArtistId = tracks.AlbumID
-INNER JOIN albums
-ON albums.AlbumId = tracks.AlbumId;
+INNER JOIN albums 
+ON artists.ArtistId = albums.ArtistId
+INNER JOIN tracks 
+ON albums.AlbumId = tracks.AlbumId
+ORDER BY artists.Name DESC;
+
+SELECT tracks.TrackId, playlists.PlaylistId, playlist_track.TrackId
+FROM tracks
+INNER JOIN playlist_track
+ON tracks.TrackId = playlist_track.TrackId
+INNER JOIN playlists
+ON playlist_track.PlaylistId = playlists.PlaylistId
+
+SELECT employees.FirstName
+FROM employees
+
+SELECT genres.Name
+FROM genres
+
