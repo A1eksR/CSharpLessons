ecb711e9c67bc3b503fe5f6167b9aa1b71adc5ca
A1eksR <147248309+A1eksR@users.noreply.github.com>
2023-12-23 13:43:13 +0200

    dsa

---
+++ Day17/Day17/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day17;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ day18/day18/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace day18;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Izveleties();
+        Console.ReadLine();
+    }
+
+    static void Izveleties()
+    {
+        Console.WriteLine("ko gribi izvadit : ");
+        Console.WriteLine("1- klientu skaitu, ");
+        Console.WriteLine("2- garako dziesmu, ");
+        Console.WriteLine("3- klientu vardu un uzvardu un dziesmas kuras vini ir nopirkusi. ");
+        int izvele = Convert.ToInt32(Console.ReadLine());
+
+        if(izvele == 1)
+        {
+            Console.WriteLine();
+        }
+        else if(izvele == 2)
+        {
+            Console.WriteLine();
+        }
+        else if(izvele == 3)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
