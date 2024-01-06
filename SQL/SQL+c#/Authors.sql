cadc7c3423b8cfd37e18cb5a7b8f791f49ec402f
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-01-06 17:07:49 +0200

    dew

---
+++ SQL/SQL+c#/Authors.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Autors(
+    Id INTEGER,
+    Vards TEXT(20),
+    Uzvards TEXT(20)
+);
+    
+INSERT INTO Autors(Id, Vards, Uzvards)
+VALUES(1, 'Janis', 'Berzins');     
+
+INSERT INTO Autors(Id, Vards, Uzvards)
+VALUES(2, 'Arturs', 'Ozolins');
+
+INSERT INTO Autors(Id, Vards, Uzvards)
+VALUES(3, 'Maris', 'Kalnins');
