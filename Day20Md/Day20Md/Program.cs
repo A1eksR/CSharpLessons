dd39b864745a4d4af97e16c323b92e58c9cc879a
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-01-13 16:17:03 +0200

    dwa

--- Day20Md/Day20Md/Program.cs
+++ Day20Md/Day20Md/Program.cs
@@ -7,16 +7,22 @@ class Program
     static void Main(string[] args)
     {
         Connections connection = new Connections();
-        Queries.CreateBookList(connection.conn);
+        List<Book> books = new List<Book>();
+        
 
         Console.WriteLine("Ko gribi darit :");
         Console.WriteLine("1: pievienot jaunu gramatu, 2: paradit visas gramatas");
         int izvele = Convert.ToInt32(Console.ReadLine());
+
+        int jaunaisId = Convert.ToInt32(Console.ReadLine());
+        String jaunaisNosaukums = Console.ReadLine();
+        int jaunaisAuthorId = Convert.ToInt32(Console.ReadLine());
+
         if(izvele == 1)
         {
-            int id = -1;
-            String nosaukums = "";
-            int authorId = -1;
+            int id = jaunaisId;
+            String nosaukums = jaunaisNosaukums;
+            int authorId = jaunaisAuthorId;
 
             Queries.InsertBook(connection.conn, id, nosaukums, authorId);
         }
