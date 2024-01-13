83d1bcb82ede3d4ef660191396934706a29425eb
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-01-13 16:00:48 +0200

    ea

--- Day20Md/Day20Md/Queries.cs
+++ Day20Md/Day20Md/Queries.cs
@@ -11,8 +11,8 @@ namespace Day20Md
 
             SqliteDataReader sQLiteDataReader;
             SqliteCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT Books.Id, Books.Nosaukums, Books.AuthorId " +
-                "FROM Books;";
+            cmd.CommandText = "SELECT Books.Id, Books.Nosaukums, Books.AuthorId, Authors.Vards, Authors.Uzvards" +
+                "FROM Books, Authors;";
 
             sQLiteDataReader = cmd.ExecuteReader();
             while (sQLiteDataReader.Read())
@@ -20,23 +20,30 @@ namespace Day20Md
                 int id = sQLiteDataReader.GetInt32(0);
                 String nosaukums = sQLiteDataReader.GetString(1);
                 int authorId = sQLiteDataReader.GetInt32(2);
+                String vards = sQLiteDataReader.GetString(3);
+                String uzvards = sQLiteDataReader.GetString(4);
 
-                books.Add(new Book(id, nosaukums, authorId));
+                books.Add(new Book(id, nosaukums, authorId, vards, uzvards));
             }
 
             return books;
         }
 
-        public static void InsertBook(SqliteConnection conn, int id, String nosaukums, int authorId)
+        public static void InsertBook(SqliteConnection conn, int id, String nosaukums, int authorId, String vards, String uzvards)
         {
             SqliteCommand cmd = conn.CreateCommand();
 
             cmd.CommandText = "INSERT INTO Books(Id, Nosaukums, AuthorId) " +
-                "VALUES (@Id, @Nosaukums, @AuthorId)";
+                "VALUES (@Id, @Nosaukums, @AuthorId)" +
+                "INSERT INTO Authors(Vards, Uzvards) " +
+                "VALUES (@Vards, @Uzvards)";
+            
 
             cmd.Parameters.AddWithValue("@Id", id);
             cmd.Parameters.AddWithValue("@Nosaukums", nosaukums);
             cmd.Parameters.AddWithValue("@AuthorId", authorId);
+            cmd.Parameters.AddWithValue("@Vards", vards);
+            cmd.Parameters.AddWithValue("@Uzvards", uzvards);
 
             try
             {
