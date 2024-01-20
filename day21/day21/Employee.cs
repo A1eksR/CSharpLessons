48dd453504b59eeed6ae3eb1b02a6949c481c389
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-01-20 16:13:39 +0200

    ds

---
+++ day21/day21/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace day21
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public String vards { get; set; }
+        public String uzvards { get; set; }
+        public String pilseta { get; set; }
+        public String valsts { get; set; }
+
+        public Employee(int id, String vards, String uzvards, String pilseta, String valsts)
+        {
+            this.id = id;
+            this.vards = vards;
+            this.uzvards = uzvards;
+            this.pilseta = pilseta;
+            this.valsts = valsts;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("id: " + id + ", vards: " + vards + ", uzvards: " + uzvards + ", pilseta: " + pilseta + ", valsts: " + valsts);
+            Console.WriteLine("-----------------");
+        }
+    }
+}
+
---
+++ day21/day21/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+
+namespace day21;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadFile();
+        Employee em = new Employee(1, "Janis", "Berzins", "Riga", "Latvia");
+        String json = JsonSerializer.Serialize(em);
+
+        List<Employee> employees = new List<Employee>();
+        employees.Add(em);
+        employees.Add(new Employee(2, "Peteris", "Liepa", "Riga", "Latvia"));
+        employees.Add(new Employee(3, "Anna", "Ozola", "Riga", "Latvia"));
+        employees.Add(new Employee(4, "Liga", "Kaktina", "Riga", "Latvia"));
+
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        String json2 = JsonSerializer.Serialize(employees, options);
+        List<Employee> employees2 = JsonSerializer.Deserialize<List<Employee>>(json2);
+        foreach (Employee emp in employees2)
+        {
+            emp.Print();
+        }
+
+
+        Console.ReadLine();
+
+    }
+    public static void ReadFile()
+    {
+        String filename = "json.txt";
+        try
+        {
+            String desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            String fullName = Path.Combine(desktopPath, filename);
+
+            StreamReader sr = new StreamReader(fullName);
+
+            String line = sr.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error!");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
