5da124c9f1b78181858022f31ddabd4cf8cb8561
Martins Leitarts <21012644+TechbearCoding@users.noreply.github.com>
2024-01-27 13:33:07 +0200

    fixers

---
+++ day21/day21/FileRead.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace day21
+{
+    public class FileRead
+    {
+        public static List<Employee> ReadFile()
+        {
+            List<Employee> list = new List<Employee>();
+            String filename = "data.json";
+            try
+            {
+                String desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                String fullName = Path.Combine(desktopPath, filename);
+
+                StreamReader sr = new StreamReader(fullName);
+
+                
+                String line = sr.ReadLine();
+                String json = "";
+
+                while (line != null)
+                {
+                    json = json + line;
+                    line = sr.ReadLine();
+                }
+
+                list = JsonSerializer.Deserialize<List<Employee>>(json);
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine(ex.Message);
+            }
+            return list;
+        }
+    }
+}
--- day21/day21/Program.cs
+++ day21/day21/Program.cs
@@ -9,51 +9,16 @@ class Program
     static void Main(string[] args)
     {
 
-        ReadFile();
+        List<Employee> lst = FileRead.ReadFile();
         Connection connection = new Connection();
 
-        Employee emp1 = new Employee(99, "arturs", "Abolins", "Riga", "Latvija");
-        String json = JsonSerializer.Serialize(emp1);
-        List<Employee> employees = new List<Employee>();
-        employees.Add(emp1);
-
-        Queries.InsertEmp(connection.conn, emp1);
-
-        
-
+        foreach(var emp in lst)
+        {
+            Queries.InsertEmp(connection.conn, emp);
+        }
 
         Console.ReadLine();
     }
-    public static void ReadFile()
-    {
-        String filename = "json.txt";
-        try
-        {
-            String desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            String fullName = Path.Combine(desktopPath, filename);
-
-            StreamReader sr = new StreamReader(fullName);
-
-            String line = sr.ReadLine();
-
-            while (line != null)
-            {
-                Console.WriteLine(line);
-                line = sr.ReadLine();
-            }
-
-            
-
-            
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error!");
-            Console.WriteLine(ex.Message);
-        }
-    }
-
-    
-    
+      
 }
 
