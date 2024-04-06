04841d61049e0fd94d81eddf0b933ae7107a1687
Martins Leitarts <martins.leitarts@kleintech.net>
2024-04-06 14:27:51 +0300

    Sample

---
+++ Day24_rest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day24_rest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService webService = new WebService();
+            webService.GetPosts();
+        }
+    }
+}
---
+++ Day24_rest/WebService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24_rest
+{
+    public class WebService
+    {
+        public void GetPosts()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/posts");
+            requestMessage.Headers.Add("Accept", "application/json");
+
+            var client = new HttpClient();
+            var response = client.Send(requestMessage);
+
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
