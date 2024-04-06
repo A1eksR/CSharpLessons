376690586f86da0a1260fe73ac51f551f04de5e7
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-04-06 14:43:52 +0300

    get comment

---
+++ day24/day24/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace day24;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WebService webService = new WebService();
+        var client = new HttpClient();
+        webService.GetPosts(client);
+        webService.GetComment(client);
+        Console.ReadLine();
+    }
+}
---
+++ day24/day24/WebService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace day24
+{
+	public class WebService
+	{
+        public void GetPosts(HttpClient client)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/posts");
+            requestMessage.Headers.Add("Accept", "application/json");
+
+            
+            var response = client.Send(requestMessage);
+
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+        public void GetComment(HttpClient client)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/comments");
+            requestMessage.Headers.Add("Accept", "application/json");
+
+            var response = client.Send(requestMessage);
+
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            
+        }
+    }
+}
+
