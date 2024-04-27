193b3483149ecf57746718751aead6b492f46ff7
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-04-27 14:17:55 +0300

    Uzd2

--- day25/day25/Program.cs
+++ day25/day25/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        List<int> listReverse = Uzd2(list);
+        for(int i = 0; i < listReverse.Count; i++)
+        {
+            Console.WriteLine(listReverse[i]);
+        }
+        Console.ReadLine();
+
+    }
+    static List<int> Uzd2(List<int> list)
+    {
+        List<int> listReverse = new List<int>();
+        for(int i = 0; i > list.Count; i--)
+        {
+            listReverse.Add(list[i]);
+        }
+        return listReverse;
+    }
+
+
+    /*static void Uzd1()
+    {
         try
         {
             Console.WriteLine("Ievadi skaitli!");
@@ -19,7 +45,6 @@ class Program
         {
             Console.WriteLine("Error");
         }
-
-    }
+    }*/
 }
 
