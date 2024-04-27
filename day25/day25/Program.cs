22c179df25f323d3d49a014e1ed6cadaa2a9ff54
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-04-27 14:49:14 +0300

    uzd3

--- day25/day25/Program.cs
+++ day25/day25/Program.cs
@@ -4,28 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
+        Uzd3Dala1();
+        int rezultats = Uzd3Dala2();
+        Console.WriteLine("skaitlis : " + rezultats);
+    }
+
+    static void Uzd3Dala1()
+    {
+        Console.WriteLine("Ievadi skaitli!");
+        int skaitlis1 = Convert.ToInt32(Console.ReadLine());
+
+
+    }
+    static int Uzd3Dala2()
+    {
+        Console.WriteLine("Ievadi skaitli!");
+        
+        bool nepareizaVertiba = true;
+        int rezultats = 0;
+        while(nepareizaVertiba = true)
+        {
+            try
+            {
+                int skaitlis2 = Convert.ToInt32(Console.ReadLine());
+                
+                nepareizaVertiba = false;
+                rezultats = skaitlis2;
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+
+            }
+            
+        }
+        return rezultats;
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    /*static List<int> Uzd2(List<int> list)
+    {
         List<int> list = new List<int>();
         list.Add(1);
         list.Add(2);
         list.Add(3);
         list.Add(4);
         List<int> listReverse = Uzd2(list);
-        for(int i = 0; i < listReverse.Count; i++)
+        for (int i = 0; i < listReverse.Count; i++)
         {
             Console.WriteLine(listReverse[i]);
         }
         Console.ReadLine();
-
-    }
-    static List<int> Uzd2(List<int> list)
-    {
+        //----------------------------------
         List<int> listReverse = new List<int>();
-        for(int i = list.Count-1; i >= 0; i--)
+        for(int i = list.Count -1; i > -1; i--)
         {
             listReverse.Add(list[i]);
         }
         return listReverse;
-    }
+    }*/
 
 
     /*static void Uzd1()
