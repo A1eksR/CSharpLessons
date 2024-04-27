fcc7126aa22f814a15b5e3b0c57654653cf454c7
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-04-27 13:47:32 +0300

    27.04 #1

---
+++ day25/day25/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace day25;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Ievadi skaitli!");
+            int ievaditaisSkaitlis = Convert.ToInt32(Console.ReadLine());
+            if (ievaditaisSkaitlis < 1)
+            {
+                Console.WriteLine("Error");
+            }
+            Task.IzvaditSkaitli(ievaditaisSkaitlis);
+            Console.ReadLine();
+        }
+        catch
+        {
+            Console.WriteLine("Error");
+        }
+
+    }
+}
+
---
+++ day25/day25/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace day25
+{ 
+    public class Task
+	{
+        public static void IzvaditSkaitli(int ievaditaisSkaitlis)
+        {
+            String simblos = "$";
+            String rezultats = " ";
+            for (int i = 0; i < ievaditaisSkaitlis; i++)
+            {
+                rezultats = rezultats + simblos;
+                Console.WriteLine(rezultats);
+            }
+            
+        }
+    }
+}
+
