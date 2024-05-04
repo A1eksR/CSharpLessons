8419c74c4e472740ebd98601d2f0fa95deb67d77
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-05-04 13:07:39 +0300

    md

---
+++ Day25Md/Day25Md/Calculations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Day25Md
+{
+	public class Calculations
+	{
+        public static int Faktorials(int skaitlis)
+        {
+            int result1 = 1;
+            for (int i = 1; i < skaitlis; i++)
+            {
+                result1 = result1 * i;
+                
+            }
+            return result1;
+
+        }
+
+
+        public static int Kapinasana(int skaitlis)
+        {
+            int result2 = 1; ;
+            for (int i = 0; i <= skaitlis - 1; i++)
+            {
+                result2 = result2 * skaitlis;
+            }
+            return result2;
+        }
+
+
+    }
+}
+
---
+++ Day25Md/Day25Md/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day25Md;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Ievadi skaitli!");
+            int skaitlis = Convert.ToInt32(Console.ReadLine());
+            int faktorials = Calculations.Faktorials(skaitlis);
+            int kapinasana = Calculations.Kapinasana(skaitlis);
+            Console.WriteLine("Faktorials: " + faktorials + " Kapinasana: " + kapinasana);
+            
+        }
+        catch
+        {
+            Console.WriteLine("Error");
+        }
+        Console.ReadLine();
+
+        
+        
+    }
+}
+
