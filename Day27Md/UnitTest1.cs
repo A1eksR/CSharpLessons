cd78fed0dbb9a6584e856c1ad7ca14a0a880417f
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-06-08 14:47:47 +0300

    day27v2

---
+++ Day27Md/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Day27Md;
+
+namespace Day27MdTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestLielako()
+    {
+        List<String> listTest = new List<String>();
+
+        listTest.Add("a");
+        listTest.Add("bb");
+        listTest.Add("ccc");
+
+        int expected = 3;
+        int result = MD.AtgrieztGarakaGarumu(listTest);
+
+        Assert.Equal(expected, result);
+    }
+}
